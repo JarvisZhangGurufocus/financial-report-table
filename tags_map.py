3ac57e39d8e8d9843ba18f6c853a310c35ae4eef
jiawei zhang <zhangjw.uta@gmail.com>
2019-08-21 15:26:49 -0500

    add stock

---
+++ tags_map.py
@@ -0,0 +1,7 @@
+TagsChanges = [
+  'Americas net sales': 'Americas',
+  'Europe net sales': 'Europe',
+  'Japan Net Sales': 'Japan',
+  'Asia-Pacific net sales': 'Rest Of Asia Pacific',
+  'Change Americas': 'Change',
+]
